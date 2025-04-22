4fb9a9a199d4339ac3b6c7340b1ffd05a6968f9c
SuSandarKyaw <loeysu123@gmail.com>
2025-04-22 21:37:46 +0630

    to fetch notifications

--- src/pages/tutor/dashboard/Dashboard.jsx
+++ src/pages/tutor/dashboard/Dashboard.jsx
@@ -261,7 +261,7 @@ const Dashboard = () => {
                         transform: `translateX(${-index * 10}px)`,
                         zIndex: item.participants.length - index,
                       }}
-                      src={`data:image/jpeg;base64,${participant.image}`}
+                      src={`data:image/jpeg;base64,${participant.avatar}`}
                       // src="https://i.pinimg.com/736x/69/8e/34/698e34d4501ab531775c23fb2fbe351c.jpg"
                       alt="Student"
                     />
